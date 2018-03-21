d2d0cb316dd61a4dfe868d8adeb0ed762fb32be6
yaohaobin <haobin.yao@gmail.com>
2018-03-21 20:53:59 +0800

    testwaste

---
+++ testwaste.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<map>
+#include<stack>
+#include<algorithm>
+#include<sstream>
+#include<stdlib.h>
+
+
+#include <sdsl/suffix_trees.hpp>
+#define N 2
+
+using namespace std;
+using namespace sdsl;
+
+
+
